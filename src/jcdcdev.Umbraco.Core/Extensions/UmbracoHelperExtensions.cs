67920793cbce8ee7579bf102ebbf77a4a9f5ca09
jcdcdev <james@jcdc.dev>
2024-04-08 00:23:10 +0100

    feature: UmbracoHelperExtensions

---
+++ src/jcdcdev.Umbraco.Core/Extensions/UmbracoHelperExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Umbraco.Cms.Web.Common;
+
+namespace jcdcdev.Umbraco.Core.Extensions;
+
+public static class UmbracoHelperExtensions
+{
+    public static UmbracoHelper GetRequiredUmbracoHelper(this IUmbracoHelperAccessor umbracoHelperAccessor)
+    {
+        umbracoHelperAccessor.TryGetUmbracoHelper(out var helper);
+        return helper ?? throw new Exception("Failed to get Umbraco Helper");
+    }
+}
