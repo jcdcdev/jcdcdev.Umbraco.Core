a90c5b60af709ca138aacd982a5a2af24212ab83
jcdcdev <james@jcdc.dev>
2024-06-24 20:13:33 +0100

    feature: dashboard manifest model

---
+++ src/jcdcdev.Umbraco.Core/Web/Models/Manifests/DashboardManifest.cs
@@ -0,0 +1,18 @@
+﻿namespace jcdcdev.Umbraco.Core.Web.Models.Manifests;
+
+public class DashboardManifest
+{
+    public string Type { get; set; }
+    public string Alias { get; set; }
+    public string Name { get; set; }
+    public string ElementName { get; set; }
+    public string Element { get; set; }
+    public int Weight { get; set; }
+    public MetaManifest Meta { get; set; }
+    public ConditionManifest[] Conditions { get; set; }
+    public class MetaManifest
+    {
+        public string Label { get; set; }
+        public string Pathname { get; set; }
+    }
+}
