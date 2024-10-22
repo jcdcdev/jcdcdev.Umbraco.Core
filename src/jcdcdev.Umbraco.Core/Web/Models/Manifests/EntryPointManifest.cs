84b8d4cce7bffd7615d780dceb9d848086fa6e90
jcdcdev <james@jcdc.dev>
2024-10-22 19:48:36 +0100

    refactor: move entryPoint to backofficeEntryPoint manifest

--- src/jcdcdev.Umbraco.Core/Web/Models/Manifests/EntryPointManifest.cs
+++ src/jcdcdev.Umbraco.Core/Web/Models/Manifests/EntryPointManifest.cs
@@ -5,5 +5,5 @@ public class EntryPointManifest : IManifest
     public required string Name { get; set; }
     public required string Alias { get; set; }
     public required string Js { get; set; }
-    public string Type => "entryPoint";
+    public string Type => "backofficeEntryPoint";
 }
