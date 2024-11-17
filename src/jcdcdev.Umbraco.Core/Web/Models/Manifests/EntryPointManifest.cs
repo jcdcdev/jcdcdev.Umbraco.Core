a12b316144d4fb3ca753b16c1b2785e2d56d0d93
jcdcdev <james@jcdc.dev>
2024-11-17 19:57:45 +0000

    refactor: EntryPointManifest extends BackofficeEntryPointManifest

--- src/jcdcdev.Umbraco.Core/Web/Models/Manifests/EntryPointManifest.cs
+++ src/jcdcdev.Umbraco.Core/Web/Models/Manifests/EntryPointManifest.cs
@@ -1,10 +1,6 @@
 namespace jcdcdev.Umbraco.Core.Web.Models.Manifests;
 
 [Obsolete("Use BackofficeEntryPointManifest instead")]
-public class EntryPointManifest : IManifest
+public class EntryPointManifest : BackofficeEntryPointManifest
 {
-    public required string Name { get; set; }
-    public required string Alias { get; set; }
-    public required string Js { get; set; }
-    public string Type => "backofficeEntryPoint";
 }
