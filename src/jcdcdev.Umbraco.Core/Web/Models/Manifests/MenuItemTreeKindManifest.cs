bc69d6bb9d0ea3e12cae3bb3df1860af3b517f0c
jcdcdev <james@jcdc.dev>
2025-07-02 00:28:53 +0100

    feature: add various Manifest classes

---
+++ src/jcdcdev.Umbraco.Core/Web/Models/Manifests/MenuItemTreeKindManifest.cs
@@ -0,0 +1,19 @@
+﻿namespace jcdcdev.Umbraco.Core.Web.Models.Manifests;
+
+public class MenuItemTreeKindManifest : IManifest
+{
+    public string Type => "menuItem";
+    public string Kind { get; set; } = "tree";
+    public required string Alias { get; set; }
+    public required string Name { get; set; }
+    public int Weight { get; set; } = 400;
+    public MenuItemTreeKindManifestMeta Meta { get; set; } = new();
+
+    public class MenuItemTreeKindManifestMeta
+    {
+        public string? Label { get; set; }
+        public string? TreeAlias { get; set; }
+        public string[] Menus { get; set; } = [];
+        public string? Icon { get; set; }
+    }
+}
