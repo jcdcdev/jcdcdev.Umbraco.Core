00574dc1906ad3e1b0ba5769c86bcf40a27f2c56
jcdcdev <james@jcdc.dev>
2025-07-06 17:36:01 +0100

    feature: add various Manifest classes

---
+++ src/jcdcdev.Umbraco.Core/Web/Models/Manifests/TreeManifest.cs
@@ -0,0 +1,17 @@
+﻿namespace jcdcdev.Umbraco.Core.Web.Models.Manifests;
+
+public class TreeManifest : IManifest
+{
+    public string Type => "tree";
+    public string? Kind { get; set; }
+    public required string Alias { get; set; }
+    public required string Name { get; set; }
+    public required TreeManifestMeta Meta { get; set; }
+    
+    public class TreeManifestMeta
+    {
+        public required string RepositoryAlias { get; set; }
+        public string? RootEntityType { get; set; }
+        public string? EntityType { get; set; }
+    }
+}
